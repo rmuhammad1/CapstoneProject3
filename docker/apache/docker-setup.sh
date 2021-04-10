567a6743571c8802e47818f970c2d15f6f448ccf
Rashid Muhammad <68817067+rmuhammad1@users.noreply.github.com>
2021-04-10 12:05:16 -0400

    Update docker-setup.sh

--- docker/apache/docker-setup.sh
+++ docker/apache/docker-setup.sh
@@ -12,9 +12,9 @@ cd $(mktemp -d /tmp/rmuhammad/Simplidocker.XXXXXXXX)
 curl https://raw.githubusercontent.com/rmuhammad1/CapstoneProject3/main/docker/apache/Dockerfile --output Dockerfile
 
 # Build image
-docker build -t centosapache .
+##docker build -t centosapache .
 
 # Run container
-docker run --name dev   -d -p 2018:80 centosapache
-docker run --name stage -d -p 2019:80 centosapache
-docker run --name prod  -d -p 2020:80 centosapache
+##docker run --name dev   -d -p 2018:80 centosapache
+##docker run --name stage -d -p 2019:80 centosapache
+##docker run --name prod  -d -p 2020:80 centosapache
