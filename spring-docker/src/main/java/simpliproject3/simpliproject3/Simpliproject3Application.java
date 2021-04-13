7e2ff1d755febe6dc78034a672eb7522002530cd
Rashid Muhammad <68817067+rmuhammad1@users.noreply.github.com>
2021-04-12 22:50:42 -0400

    Add files via upload

---
+++ spring-docker/src/main/java/simpliproject3/simpliproject3/Simpliproject3Application.java
@@ -0,0 +1,13 @@
+package simpliproject3.simpliproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simpliproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simpliproject3Application.class, args);
+	}
+
+}
---
+++ spring-docker/src/test/java/simpliproject3/simpliproject3/Simpliproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package simpliproject3.simpliproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simpliproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
